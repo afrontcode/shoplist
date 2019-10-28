084c10452bb2c08f3d8e63e7189ae4137622d94e
Andrea <slandrea00@gmail.com>
2019-10-28 10:56:36 -0500

    first commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+new Vue({
+	el: '#app',
+	data: {
+		titulo : 'My Shopping List',
+		frutas : [
+		{nombre: 'Manzana',cantidad: 5},	
+		{nombre: 'Pera',cantidad: 0},	
+		{nombre: 'Durazno',cantidad: 9}
+		],
+		nuevafruta : '',
+		total : 0 
+	},
+    methods:{
+      agregarfruta (){
+        this.frutas.push({
+          nombre: this.nuevafruta, cantidad :1
+        });
+        this.nuevafruta='';
+      },
+      borrarfruta: function(index){
+         this.frutas.splice(index, 1)
+      },
+    }
+  
+})
